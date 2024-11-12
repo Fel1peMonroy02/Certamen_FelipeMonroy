f468f4483df189d42a5a289f089696b208d55925
aaa <felipemonroy0511@gmail.com>
2024-11-12 15:48:28 -0300

    Commit #3 Finalizado

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Button CalcularIMC;
 
         TextView MensajeFinal, MensajeIMC;
-        
+
         Sexo = findViewById(R.id.spner_Sexo);
 
         Peso = findViewById(R.id.txtPeso);
